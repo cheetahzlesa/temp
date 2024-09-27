0e405cb57ad5ea188e6365fd6edc16b44fbae45e
Gabriel Kozuch <kozuch8@uniba.sk>
2024-09-27 15:05:11 +0200

    hanoi hopefuly repaired

--- hanoi.c
+++ hanoi.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-void hanoi(int n, int a, int b, int c)
+void hanoi(int n, int a, int b)
 {
   if (n > 0)
   {
-    hanoi(n - 1, a, c, b);
+    hanoi(n - 1, a, b);
     printf("move stone %d from %d to %d\n", n, a, b);
-    hanoi(n- 1  , c, b, a);
+    hanoi(n- 1  , b, a);
   }
 }
 
 int main()
 {
   int n = 4;
-  hanoi(n, 1, 2, 3);
+  hanoi(n, 1, 2);
   return 0;
 }
